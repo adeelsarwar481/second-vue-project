b2a53b96718a7c1266aa7a7de2633dd161996f4b
adeelsarwar481 <adeel.sarwar@innov8.jp>
2026-03-31 14:35:58 +0500

    2nd commit in 3rd branch

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
   <h2> text deleting environment variable</h2>
   <h3> after merging creating 3rd branch and pushing code for testing</h3>
   <h4> 2nd commit in 3rd branch</h4>
+  <h1>creating a dev branch</h1>
 </template>
 
 <style scoped></style>
